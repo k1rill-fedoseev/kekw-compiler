9204d750384a58d6e1260c6377dd5b262299eda8
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-08-23 23:35:24 +0300

    Add pretty print

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,12 @@ public class Main {
             System.err.println("Syntax parsing failed");
             System.exit(1);
         }
-        System.out.println(ast);
+        StringBuilder res = new StringBuilder("ElementsList[\n");
+        for (IElement elem: ast) {
+            res.append("  ").append(elem).append(",\n\n");
+        }
+        res.append("]");
+        System.out.println(res.toString());
         Interpreter interpreter = new Interpreter(new Scope());
 
         for (IElement elem: ast) {
