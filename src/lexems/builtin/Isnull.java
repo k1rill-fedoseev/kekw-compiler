6f290cc7a3eeabe27e9e49d91c2a1b28dbc86266
Leonid Tyurin <lenya_tyurin@mail.ru>
2020-08-31 18:16:42 +0300

    Add less && isnull

---
+++ src/lexems/builtin/Isnull.java
@@ -0,0 +1,33 @@
+package lexems.builtin;
+
+import lexems.*;
+
+import java.util.List;
+
+public class Isnull extends Func implements IBuiltin {
+    public Isnull() {
+        super(
+                new Atom("isnull"),
+                List.of(new Atom("n1")),
+                new ElementsList()
+        );
+    }
+
+    public IElement execute(List<IElement> argValues) {
+        if (argValues.size() != 1){
+            // error
+            return null;
+        }
+
+        IElement f = argValues.get(0);
+
+//        System.out.println(f);
+
+        if (f instanceof ElementsList) {
+            return new BooleanLiteral(((ElementsList) f).isEmpty());
+        } else {
+            return null;
+        }
+
+    }
+}
